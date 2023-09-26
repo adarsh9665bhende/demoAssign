ff8498efd59c63bb62df8c3ffd143edb8fff7e2a
adarsh9665bhende <105604143+adarsh9665bhende@users.noreply.github.com>
2023-09-26 22:33:56 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,81 @@
+import socket
+import http.server
+
+keyValueDict = dict()
+
+
+#ip addresses and ports
+serverIP=str(input("Enter Server IP: "))
+dst_ip=serverIP
+dport="123456"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print ("Socket successfully created")
+
+s.bind((dst_ip, dport))
+print ("socket binded to %s" %(dport))
+
+s.listen(5)
+print ("socket is listening")
+
+while True:
+  
+  try:
+    c, addr = s.accept()
+    print ('Got connection from: ', addr )
+
+    while True:
+      recvmsg = c.recv(1024).decode()
+      print('Server received: '+ recvmsg)
+      #message has be recieved
+
+      #if client quits the session, conncection gets disconnected
+      if recvmsg == "quit":
+        print('Disconnected : Client has disconnected')
+        break
+      #if recvmsg == "KeyboardInterrupt":
+       # print('Disconnected : Client disconnected due to an interrupt')
+       # break
+      if recvmsg == "":
+        break
+      
+      #traverse recieved message
+      recv_request = recvmsg.split(" ")
+      recv_func = recv_request[0]
+      recv_directory = recv_request[1]
+
+
+      #functions GET , PUT , DELETE
+      if recv_func == "GET":
+        recv_key = recv_directory.split("=")[1]
+        if recv_key in keyValueDict :
+          msg = "HTTP/1.1 200 OK\r\n\r\n" + keyValueDict[recv_key]
+          c.send(msg.encode())
+        else:
+          msg = "HTTP/1.1 404 Not Found \r\n\r\n No such key exists!"
+          c.send(msg.encode())
+
+      elif recv_func == "PUT":
+        recv_key = recv_directory.split("/")[2]
+        rec_val = recv_directory.split("/")[3]
+        keyValueDict[recv_key] = rec_val
+        c.send("HTTP/1.1 200 OK\r\n\r\nPush sucess!".encode())
+
+      elif recv_func == "DELETE":
+        recv_key = recv_directory.split("/")[2]
+        if recv_key in keyValueDict:
+          keyValueDict.pop(recv_key)
+          c.send("HTTP/1.1 200 OK\r\n\r\nDelete success!".encode())
+        else:
+          c.send("HTTP/1.1 404 Not Found\r\n\r\nDelete failure!: Key does not exist".encode())
+
+          
+      else:
+        c.send("HTTP/1.1 400 Bad Request\r\n\r\nInvalid request!: Command not found".encode())
+      print("Key Val: ", keyValueDict)
+      
+    c.close()
+  except socket.error as e:
+    print("Error: ", e)
+    c.close()
+    break
