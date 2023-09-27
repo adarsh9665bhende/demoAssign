6975d565fd421bca9c04508b9af68bc267d756f1
vishal-iith <106789351+vishal-iith@users.noreply.github.com>
2023-09-27 12:46:51 +0000

    Update client.py .''

--- client.py
+++ client.py
@@ -1,88 +1,78 @@
-import http.client
+# import http.client
 #import time
 import socket
 
 serverIP = "10.0.1.2"
 
 dst_ip = str(input("Enter dstIP: "))
-s = socket.socket()
-
+#s = socket.socket()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print(dst_ip)
 
-port = 12346
+dport = 12346
 
-#here establishihed connection between destnination_ip and port
-s.connect((dst_ip, port))
+# here establish connection between destination_ip and port
+
+
+
+s.connect((dst_ip, dport))
 
 print("Successfully established Connection")  
 
 while True:
- try:
-     inputrequest = input("Entre the request:")
-     request = inputrequest
+    try:
+        inputrequest = input("Enter the request:")
+        request = inputrequest
 
-     if inputrequest=="quit" :
-      print("Ending Program:")
-      s.send('Ending Program'.encode())
-      s.close()
+        if inputrequest == "quit": 
+            print("Ending Program:")
+            s.send('Ending Program'.encode())
+            s.close()
+            break;
  
-    # spliting request by space
-     inputrequest = inputrequest.split(" ")  
+        # splitting request by space
+        inputrequest = inputrequest.split(" ")  
     
-    #for valid request the length of splited must be 3
+        # for valid request, the length of split must be 3
+        split_length = len(inputrequest)
 
-     split_lenght = len(inputrequest)
+        if split_length != 3 :
+            print("Invalid request: we will consider up to length 3")
+            continue
 
-     if split_lenght != 3 :
-      print("Invalid request: we will consider upto length 3")
-      continue
+        if inputrequest[0] == "GET":
+            inputrequest_middle = inputrequest[1]
 
-     if inputrequest[0] == "GET" :
-      inputrequest_middle = inputrequest[1]
-
-      request = "GET /assignment2/key/val HTTP/1.1\'r\n\r\n".formay(key = inputrequest_middle)
+            request = "GET /assignment2/key/val HTTP/1.1\r\n\r\n".format(key = inputrequest_middle)
     
-    #s.send(request.encode())
-    #print("Received:" + s.recv(1024).decode())
+        #s.send(request.encode())
+        #print("Received:" + s.recv(1024).decode())
 
-     elif inputrequest[0] =="PUT" :
-    # for put we have to further split the inputrequest[1]
-      inputrequest_middle =inputrequest_middle.split("/")
-    #if inputrequest_middle[1] != assignment2 invalid input
-        if inputrequest_middle[1] != assignment2 :
-         print("Invalid request:we will consider only key")
-         continue
+        elif inputrequest[0] =="PUT":
+            # for put, we have to further split the inputrequest[1]
+            inputrequest_middle = inputrequest[1].split("/")
+            # if inputrequest_middle[1] != assignment2, invalid input
+            if inputrequest_middle[1] != assignment2:
+                print("Invalid request: we will consider only key")
+                continue
 
-     request ="PUT /assignment2/key/val HTTP/1.1\r\n\r\n".format(key=inputrequest_middle[0],val =inputrequest_middle[1])
-    # s.send(request.encode())
-     #print("Received:" + s.recv(1024).decode())
+            request ="PUT /assignment2/key/val HTTP/1.1\r\n\r\n".format(key=inputrequest_middle[0],val =inputrequest_middle[1])
 
-     elif inputrequest[0] =="DELETE" :
-      inputrequest_middle = inputrequest[1]
-      request = "DELETE  /assignment2/key/val HTTP/1.1\r\n\r\n".format(key = inputrequest_middle)
-      #s.send(request.encode())
-     # print("Received:" + s.recv(1024).decode())
+        elif inputrequest[0] =="DELETE":
+            inputrequest_middle = inputrequest[1]
+            request = "DELETE  /assignment2/key/val HTTP/1.1\r\n\r\n".format(key = inputrequest_middle)
+            #s.send(request.encode())
+            # print("Received:" + s.recv(1024).decode())
     
-     else 
-      print("INvalid Request: not contain GET,PUT, or DELETE")
-      continue
+        else:
+            print("Invalid Request: does not contain GET, PUT, or DELETE")
+            continue
 
-   s.send(request.encode())
-   print("Received:" + s.recv(1024).decode())
+        s.send(request.encode())
+        print("Received:" + s.recv(1024).decode())
 
-except socket.error as error
-
-print("Connection_Closed",error)
+    except socket.error as error:
+        print("Connection Closed", error)
 
 s.close()
 #s.close()
-
-
-
-
-
-#Write your code here:
-#1. Add code to send HTTP GET / PUT / DELETE request. The request should also include KEY.
-#2. Add the code to parse the response you get from the server.
-#s.send('Hello server'.encode())
-#print ('Client received '+s.recv(1024).decode())
