ad6b3f33b906fa87d7aff0fe63a8462d2a52f722
adarsh9665bhende <105604143+adarsh9665bhende@users.noreply.github.com>
2023-09-28 02:04:21 +0530

    Update part2_client.py

--- part2_client.py
+++ part2_client.py
@@ -4,20 +4,17 @@ serverIP = "10.0.1.2"
 
 dst_ip = str(input("Enter dstIP: "))
 s = socket.socket()
-
+#S = socket.socket()
 print(dst_ip)
-
 port = 12346
 
 s.connect((dst_ip, port))
-
-
 while True:
-	htt = str(input('Enter the url: '))
+	htt = str(input('Enter the REquest: '))
 	http = htt + '\r\n\r\n'
 	s.send(http.encode())
+    #S.send(http.encode())
 	print(s.recv(1024).decode())
 
 
-#s.send('Hello server'.encode())
 #print ('Client received '+s.recv(1024).decode())
