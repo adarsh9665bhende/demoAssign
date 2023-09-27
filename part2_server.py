9596543d8f194b39ed340a49a65eb1f85b41a175
adarsh9665bhende <105604143+adarsh9665bhende@users.noreply.github.com>
2023-09-28 02:04:45 +0530

    Update part2_server.py

--- part2_server.py
+++ part2_server.py
@@ -1,39 +1,27 @@
 import socket
-
-#WRITE CODE HERE:
-#1. Create a KEY-VALUE pairs (Create a dictionary OR Maintain a text file for KEY-VALUES).
-
-
+#WRITE CODE HERE
 dst_ip = str(input("Enter Server IP: "))
 
 s = socket.socket()
-print ("Socket successfully created")
-
+#S = socket.socket()
+print ("Successful creation of socket:")
 dport = 12345
-
 s.bind((dst_ip, dport))
 print ("socket binded to %s" %(dport))
 
 s.listen(5)
 print ("socket is listening")
 c, addr = s.accept()
-print ('Got connection from', addr)
+print ('Got connection from:', addr)
+
 rcmsg = c.recv(1024).decode()
-
 map = {
-  'key1': 'val1',
-  'key2': 'val2',
-  'key3': 'val3',
-  'key4': 'val4',
-  'key5': 'val5',
-  'key6': 'val6'
+  'key1': 'val1','key2': 'val2','key3': 'val3','key4': 'val4', 'key5': 'val5','key6': 'val6'
 }
-
 while rcmsg:
 	if rcmsg in map:
-		a = map[rcmsg]
+		final_responce = map[rcmsg]
 	else:
-		a = 'Error 404: Not found'
-	c.send(a.encode())
-
+		final_responce = 'Error 404: Not found'
+	c.send(final_responce.encode())
 	rcmsg = c.recv(1024).decode()
